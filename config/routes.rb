6efe85ac8230646d1a8d43914a5a0a0e0e4b6289
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-02 14:48:37 -0700

    Generate model for comments and associate with articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
+ 
+  # below is only for GET, we can instead use the resources method to map the conventional routes (CRUD)
+  # get "/articles", to: "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

6a4be71a7f5192de29776eaea4543560c7d1499a
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-07 14:18:54 -0700

    Pass user params when creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new
+        @user = User.new(user_params)
         if @user.save
             redirect_to root_path, notice: 'Successfully registered!'
         end

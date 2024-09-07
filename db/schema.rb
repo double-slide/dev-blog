36a806e0fae63a5698a3be0c1369df312a00d34a
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-07 14:23:27 -0700

    Migration to include confirmed_at to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_06_185545) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_07_212019) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_06_185545) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

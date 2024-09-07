8ed8b33bee6f180c222ac00ad5223aebe2f6bab7
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-07 15:15:21 -0700

    Add confirmations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,11 @@ class User < ApplicationRecord
       signed_id expires_in: 15.minutes
     end
 
+    def send_confirmation_email!
+      confirmation_token = generate_confirmation_token
+      UserMailer.confirmation(self, confirmation_token).deliver_now
+    end
+
     validates :email, presence: true
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end

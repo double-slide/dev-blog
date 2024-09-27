1533875c8a74f401eb4d9e4455e67fb180f5ced0
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-26 21:53:55 -0700

    Add image upload to each article and display within article view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments, dependent: :destroy
+  # has_many :comments, dependent: :destroy
+  has_one_attached :image
+  belongs_to :user
 
   validates :title, presence: true
+  validates :user, presence: true, 
   validates :body, presence: true, length: { minimum: 10 }
 end

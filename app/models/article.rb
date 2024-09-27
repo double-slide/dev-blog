83cfda6be9dd3f99a92a22e5e45729b7f169baf9
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-26 23:47:14 -0700

    Style updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,10 @@ class Article < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+  def current_user_is_author?(current_user)
+    current_user == self.user
+  end
+
   validates :title, presence: true
   validates :user, presence: true
   validates :body, presence: true, length: { minimum: 10 }

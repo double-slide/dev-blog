91f4ee6ae242661a011eedbf810c26392f197e8a
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-26 23:26:19 -0700

    Link articles to have one user (author) and users to have many articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,6 @@ class Article < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :user, presence: true, 
+  validates :user, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_27_021703) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_050010) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_27_021703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -68,5 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_27_021703) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end

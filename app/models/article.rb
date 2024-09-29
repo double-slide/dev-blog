8315f0c39caf186c73d93f8c81c89cae25694b50
Iaan Johnston <iaan.e.jo@gmail.com>
2024-09-29 15:12:52 -0700

    Style changes & add hover tooltip for blocked article links

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,7 @@ class Article < ApplicationRecord
     current_user == self.user
   end
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 40 }
   validates :user, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
